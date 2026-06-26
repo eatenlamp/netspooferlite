4baabf172be67fce710eb360ca9ffce40f44fc30
eatenlamp <91413384+eatenlamp@users.noreply.github.com>
2026-06-26 16:47:01 +0700

    Delete customize.sh

--- customize.sh
+++
@@ -1,7 +0,0 @@
-#!/system/bin/sh
-
-ui_print "*******************************"
-ui_print "  Network Spoofer Lite v1.0"
-ui_print "  by eatenlamp"
-ui_print "*******************************"
-ui_print "  Installation successful."
